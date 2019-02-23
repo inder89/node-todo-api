54335ef4188abe9524aa4033ecc98a10a3aaaf2b
Inderpal <inderpal.chauhan@gmail.com>
2019-02-23 18:54:16 +1100

    Test the DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -59,8 +59,8 @@ app.delete('/todos/:id', (req, res) => {
         if(!todo) {
             return res.status(404).send();
         }
-        
-        res.status(200).send(todo);
+
+        res.send({todo});
     }).catch((e) => {
         res.status(400).send();
     })
